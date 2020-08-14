e91af810b3c9384062a1dec3b8ea9d751c1da2ba
sebeom <hccgahy@outlook.com>
2020-08-14 12:35:38 +0900

    update hello2

--- polls/views.py
+++ polls/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello2, world. You're at the polls index.")
